0f87f85838dabcf514efba02da2bf9c03eb809fb
Keshav Jha <keshavjha122006@gmail.com>
2025-07-31 16:40:29 +0530

    Made resume AI generation and view in console log

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,8 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 const routes = [
   index("routes/home.tsx"),
   route("/auth", "routes/auth.tsx"),
-  route("/upload", "routes/upload.tsx"), 
+  route("/upload", "routes/upload.tsx"),
+  route("/resume/:id", "routes/resume.tsx"),
 ] satisfies RouteConfig;
 
 export default routes;
